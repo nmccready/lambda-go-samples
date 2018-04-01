4da4f12a2bf8297c0cb3d76a3d28a2dee93512da
lalyos <lalyos@yahoo.com>
2018-04-01 15:31:25 +0200

    gun module to work with AWS cloudwatch logs

---
+++ cmds/aws.bash
@@ -0,0 +1,42 @@
+init() {
+    cmd-export-ns aws "aws namespace"
+    cmd-export aws-logstreams
+    cmd-export aws-logs
+    
+    #AWS=$(which aws)
+    deps-require aws
+    AWS=.gun/bin/aws
+}
+aws-loggroups() {
+  declare desc="lists aws loggroups"
+  declare groupPattern=${1}
+
+  $AWS logs describe-log-groups \
+     --query 'logGroups[?contains(logGroupName,`'${groupPattern}'`)].logGroupName' \
+     --out text
+}
+
+aws-logstreams() {
+  declare desc="lists log streams belonging to a log group pattern"
+  declare groupPattern=${1:? groupPattern required}
+
+  groupName=$(aws-loggroups $groupPattern)
+  $AWS logs describe-log-streams \
+      --log-group-name $groupName \
+      --query 'logStreams[-1].logStreamName' \
+      --out text
+}
+
+aws-logs() {
+  declare desc="list log messages belonging to a log group pattern"
+  declare groupPattern=${1:? groupPattern required}
+  declare messagePattern=${2}
+
+  groupName=$(aws-loggroups $groupPattern)
+  streamName=$(aws-logstreams $groupName)
+  $AWS logs filter-log-events \
+    --log-group-name $groupName \
+    --log-stream-names $streamName \
+    --query 'events[?contains(message,`'$messagePattern'`)].message' \
+    --out text
+}
